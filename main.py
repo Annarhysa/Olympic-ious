784a1287dd5804313a5ca1466dc89103a67e611e
Annarhysa Albert <annarhysa13@gmail.com>
2023-07-20 22:55:40 +0530

    New Changes

---
+++ main.py
@@ -0,0 +1,11 @@
+questions = ("What is the Olympic motto in Latin and what does it mean in English?",
+             "Which athlete has won the most gold medals in Olympic history?",
+             "Which city has hosted the most Olympic Games?",
+             "In which sport did the 'Miracle on Ice' take place during the 1980 Winter Olympics?")
+options = (("Citius, Altius, Fortius (Faster, Higher, Stronger)", "xyz", "abc", "123"), 
+           ("Michael Phelps", "Prithvi Shah", "Usain Bolt", "Fedrer"),
+           (), ())
+answers = ()
+guesses = []
+score = 0
+question_num = 0
