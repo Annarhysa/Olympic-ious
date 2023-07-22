40d71e4ddc4e8ef9eb48708ceae9fa9682c3eaf4
Annarhysa Albert <annarhysa13@gmail.com>
2023-07-22 19:06:23 +0530

    New Changes

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/quiz")
 def quiz():
-    return "Test quiz"
+    return render_template("quiz.html")
+
+@app.route("/submitquiz", methods=['POST', 'GET'])
+def submit():
+    value = request.form['option']
+    return value
 
 if __name__ == "__main__":
     app.run(debug=True)
