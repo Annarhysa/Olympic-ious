4764a2ae5e01169606dcbbcb9cdd066bb2c7a30e
Annarhysa Albert <annarhysa13@gmail.com>
2023-07-22 19:38:53 +0530

    New Changes

--- main.py
+++ main.py
@@ -23,11 +23,19 @@ class Question:
              return self.option1
         elif self.correctOption == 2:
             return self.option2
+        elif self.correctOption == 3:
+            return self.option3
 
+q1 = Question(1, "What is the Olympic motto in Latin and what does it mean in English?", "Citius, Altius, Fortius (Faster, Higher, Stronger)", "xyz", "abc", 1)
+q2 = Question(2, "Which athlete has won the most gold medals in Olympic history?", "Michael Phelps", "Prithvi Shah", "Usain Bolt", 1)
+q3 = Question(3, "Which city has hosted the most Olympic Games?", "London, UK", "Bejin, Japan", "Paris, France", 1)
+q4 = Question(4, "In which sport did the 'Miracle on Ice' take place during the 1980 Winter Olympics?", "Ice Skating", "Volleyball", "Ice Hockey", 3)
+
+questions_list = [q1, q2, q3, q4]
 
 @app.route("/quiz")
 def quiz():
-    return render_template("quiz.html")
+    return render_template("quiz.html", questions_list = questions_list)
 
 @app.route("/submitquiz", methods=['POST', 'GET'])
 def submit():
