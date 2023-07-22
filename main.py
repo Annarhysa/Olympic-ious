476134d541216b5c8adfcdedf69c2b67cc9a0b0a
Annarhysa Albert <annarhysa13@gmail.com>
2023-07-22 18:42:40 +0530

    New Changes

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-questions = ("What is the Olympic motto in Latin and what does it mean in English?",
-             "Which athlete has won the most gold medals in Olympic history?",
-             "Which city has hosted the most Olympic Games?",
-             "In which sport did the 'Miracle on Ice' take place during the 1980 Winter Olympics?")
-options = (("A. Citius, Altius, Fortius (Faster, Higher, Stronger)", "B. xyz", "C. abc", "D. 123"), 
-           ("A. Michael Phelps", "B. Prithvi Shah", "C. Usain Bolt", "D. Fedrer"),
-           ("A. London, UK", "B. Beijin, Japan", "C. Paris, Japan", "D. Kolkata, India"), 
-           ("A. Polo", "B. Golf", "C. Ice Skating", "D. Ice Hockey"))
-answers = ("A", "A", "A", "D")
-guesses = []
-score = 0
-question_num = 0
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/quiz")
+def quiz():
+    return "Test quiz"
+
+if __name__ == "__main__":
+    app.run(debug=True)
